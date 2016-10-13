d4fdd4be81e42f5de2098d7fd759a3e56cbe57dc
Ricardo Schmidt <r.schmidt@queo-group.com>
2016-10-13 13:32:48 +0200

    Cleanup styleguide sources due to injection from dependency

---
+++ app/templates/src/javascripts/inject.js
@@ -0,0 +1,2 @@
+'use strict';
+require('styleguide/src/javascripts/inject');
--- app/templates/src/javascripts/shared.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-window['hljs'] = require('highlightjs/highlight.pack');
-require('./shared/highlight');
-require('./shared/toggle');
